5fc3fc165868c26d135dd07d5abb7d1e80224eba
Nithya Natarajan <nithyanatarajn@gmail.com>
2015-06-27 21:55:28 +0530

    [Nithya] Adding / endpoint and temperature endpoint

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,27 +1,5 @@
-var user_helper = require('../helper/user_helper'),
-user_name = "user";
-
 module.exports = function(app, db) {
-  app.post('/', function(req, res) {
-    var user_object = user_helper.create_user(req, res);
-    db.save(user_name, user_object, function(err) {
-      if (err) {
-        console.log(err.message);
-        res.status(500).send('Cannot save user details');
-        return;
-      }
-      res.sendStatus(201);
-    });
-  });
-
   app.get('/', function(req, res) {
-    db.get(user_name, function(err, obj) {
-      if (err) {
-        console.log(err.message);
-        res.status(404).send('Cannot find user details, please save them first.');
-        return;
-      }
-      res.status(200).send(obj);
-    });
+    res.status(200).send('app up and running');
   });
 };
